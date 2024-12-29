f831455b3a44989d9a124fbe1aadf6c4923a22c3
Senjucasie <paulkpraveen@gmail.com>
2024-12-29 21:03:27 +0530

    Finished the logic for rescaling and repositioning the model

--- Assets/Scripts/ModelResizer.cs
+++ Assets/Scripts/ModelResizer.cs
@@ -8,6 +8,7 @@ public class ModelResizer : MonoBehaviour
     void Start()
     {
         CreateCenterPoint();
+        checksize();
     }
 
     private void CreateCenterPoint()
@@ -15,6 +16,7 @@ public class ModelResizer : MonoBehaviour
         GameObject updatedpivot = new GameObject("pivot");
         updatedpivot.transform.position = FindCenterPoint();
         transform.parent = updatedpivot.transform;
+        updatedpivot.transform.position = Vector3.zero;
     }
 
     private Vector3 FindCenterPoint()
@@ -33,6 +35,29 @@ public class ModelResizer : MonoBehaviour
         
         return bounds.center;
     }
+    private void checksize()
+    {
+        Vector3 add = Sizeofmodel();
+        Debug.Log(add);
+        Debug.Log( 1/((add.x+add.y+add.z)/3));
+    
+    }
 
+    private Vector3 Sizeofmodel()
+    {
+        Bounds bounds = new Bounds(transform.position, Vector3.zero);
+        Renderer[] renderers = GetComponentsInChildren<Renderer>();
+
+        if (renderers == null || renderers.Length == 0)
+        {
+            Debug.LogWarning($"Didnot Find any renders in this gameobject : {this.name}");
+            return Vector3.zero;
+        }
+
+        foreach (Renderer renderer in renderers)
+            bounds.Encapsulate(renderer.bounds);
+
+        return bounds.size;
+    }
     
 }
